8b8eedeb2c591945baed55ab19a49a818f8437af
Rifat Khan <rifat6633@gmail.com>
2023-10-09 12:46:13 +0600

    define of schema field as optional

--- prisma/migrations/20231009063243_modeluserv1/migration.sql
+++ prisma/migrations/20231009063243_modeluserv1/migration.sql
@@ -4,9 +4,9 @@ CREATE TYPE "UserRoles" AS ENUM ('USER', 'RECRUITER', 'ADMIN');
 -- CreateTable
 CREATE TABLE "user" (
     "id" STRING NOT NULL,
-    "username" STRING NOT NULL,
-    "email" STRING NOT NULL,
-    "hashedPassword" STRING NOT NULL,
+    "username" STRING,
+    "email" STRING,
+    "hashedPassword" STRING,
     "role" "UserRoles" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,

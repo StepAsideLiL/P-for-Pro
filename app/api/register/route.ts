b018cab84501127800d5cf3e32f0c0f9b8f51f3b
Rifat Khan <rifat6633@gmail.com>
2023-10-09 18:45:13 +0600

    revert register api code

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -9,16 +9,15 @@ export async function POST(request: Request) {
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    // const user = await prisma.user.create({
-    //   data: {
-    //     username,
-    //     email,
-    //     hashedPassword,
-    //   },
-    // });
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        hashedPassword,
+      },
+    });
 
-    // return NextResponse.json(user);
-    return NextResponse.json({ username, email, hashedPassword });
+    return NextResponse.json(user);
   } catch (err: any) {
     console.log(err, "REGISTRATION_ERROR");
     return new NextResponse("Internal Error", { status: 500 });

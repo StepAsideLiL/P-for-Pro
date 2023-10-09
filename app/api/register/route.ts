d5a0e95c5737dbe871e83141aaf97487cf10a001
Rifat Khan <rifat6633@gmail.com>
2023-10-09 17:59:34 +0600

    remove error code to pinpoint the error

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -9,15 +9,16 @@ export async function POST(request: Request) {
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const user = await prisma.user.create({
-      data: {
-        username,
-        email,
-        hashedPassword,
-      },
-    });
+    // const user = await prisma.user.create({
+    //   data: {
+    //     username,
+    //     email,
+    //     hashedPassword,
+    //   },
+    // });
 
-    return NextResponse.json(user);
+    // return NextResponse.json(user);
+    return NextResponse.json({ username, email, hashedPassword });
   } catch (err: any) {
     console.log(err, "REGISTRATION_ERROR");
     return new NextResponse("Internal Error", { status: 500 });

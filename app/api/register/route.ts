7e8f7b737f96a424b1baf5eb7b8acb1a0fd2b76f
Rifat Khan <rifat6633@gmail.com>
2023-10-09 13:59:08 +0600

    ignore typescript error on vercel production

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,25 +3,19 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/lib/prismadb";
 
-type RequestBody = {
-  username: string;
-  email: string;
-  password: string;
-};
-
 export async function POST(request: Request) {
   try {
     // const { username, email, password } = await request.json();
-    const { username, email, password } = (await request.json()) as RequestBody;
+    const { username, email, password } = await request.json();
 
-    const hashedPassword: string = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
     // const hashedPassword = password;
 
     const user = await prisma.user.create({
       data: {
-        username: username,
-        email: email,
-        hashedPassword: hashedPassword,
+        username,
+        email,
+        hashedPassword,
       },
     });
 

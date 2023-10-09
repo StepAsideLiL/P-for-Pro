437c25ddab9d6333cabf8e6b3c09bda07c77d4b3
Rifat Khan <rifat6633@gmail.com>
2023-10-09 09:12:06 +0600

    remove authOptions to different file because of this error on vercel - authOptions is not a valid Route export field.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,55 +1,51 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import { authOptions } from "@/lib/config/auth-options";
+import NextAuth from "next-auth";
 
-import prisma from "@/lib/prismadb";
+// export const authOptions: AuthOptions = {
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     CredentialsProvider({
+//       name: "credentials",
+//       credentials: {
+//         username: { label: "username", type: "text" },
+//         email: { label: "email", type: "text" },
+//         password: { label: "password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         if (!credentials?.email || !credentials?.password) {
+//           throw new Error("Invalid credentials");
+//         }
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: { label: "username", type: "text" },
-        email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid credentials");
-        }
+//         const user = await prisma.user.findUnique({
+//           where: {
+//             email: credentials.email,
+//           },
+//         });
 
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
+//         if (!user || !user?.hashedPassword) {
+//           throw new Error("Invalid credentials");
+//         }
 
-        if (!user || !user?.hashedPassword) {
-          throw new Error("Invalid credentials");
-        }
+//         const isIncorrectPassword = await bcrypt.compare(
+//           credentials.password,
+//           user.hashedPassword
+//         );
+//         // const isIncorrectPassword = true;
 
-        const isIncorrectPassword = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
-        // const isIncorrectPassword = true;
+//         if (!isIncorrectPassword) {
+//           throw new Error("Invalid credentials");
+//         }
 
-        if (!isIncorrectPassword) {
-          throw new Error("Invalid credentials");
-        }
-
-        return user;
-      },
-    }),
-  ],
-  debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+//         return user;
+//       },
+//     }),
+//   ],
+//   debug: process.env.NODE_ENV === "development",
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
 
 const handler = NextAuth(authOptions);
 
---
+++ lib/config/auth-options.ts
@@ -0,0 +1,52 @@
+import { AuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+import prisma from "@/lib/prismadb";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "username", type: "text" },
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isIncorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        // const isIncorrectPassword = true;
+
+        if (!isIncorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
